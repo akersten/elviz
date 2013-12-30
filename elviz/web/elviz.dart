f73fa5395d3527adcd5e8ceb8ad660c05ced789d
Alex Kersten <alex@dividebyxero.com>
2013-12-30 03:48:41 -0600

    Prepare for file reading and main cue timer

--- elviz/web/elviz.dart
+++ elviz/web/elviz.dart
@@ -1,11 +1,18 @@
 import 'dart:html';
 
+//XXX: State for certain actions - right now things will break if the syllable
+//button action is triggered before things are actually set up (CSS will prevent
+//this for normal users, but if someone starts poking it it'll break... Suppose
+//that's a layer-8 error though).
+
 /**
  * Set up actions for page elements.
  */
 void main() {
   querySelector("#tapCuesLink")
     ..onClick.listen(tapCues);
+  querySelector("#syllableButton")
+    ..onClick.listen(syllableClick);
 }
 
 List<String> tokens = [];
@@ -18,6 +25,8 @@ void tapCues(MouseEvent event) {
   //later, during the rendering, and leave them in the word for now (to be
   //displayed with dashes during the timecue input phase).
   TextAreaElement txta = querySelector("#lyricsInputBox");
+  //Get rid of any decorative verse slashes before doing this though...
+  txta.value = txta.value.replaceAll(new RegExp(r"/"), "\n");
   tokens = txta.value.split(new RegExp(r"[\t\n ]+")); //Fix double+ spacing too.
   
   //Generate the HTML for the timecue input list...
@@ -63,4 +72,25 @@ void tapCues(MouseEvent event) {
   }
   
   tokens.forEach(addToTheLyricsList);
+  
+  //Switch over to the second stage interface...
+  querySelector("#stage1").style.display = "none";
+  querySelector("#stage2").style.display = "block";
+}
+
+
+var syllableClickIdx = -1;
+var syllableClickIdx2 = 0;
+
+/**
+ * Triggered by the user hitting the syllable button. Add the syllable timestamp
+ * to the list of timecues, unless the index has a non-null value in the
+ * innerTimecues list, in which case set its values (using the 2nd index above).  
+ */
+void syllableClick(MouseEvent event) {
+  //XXX: Does mouseClick event also capture spacebar-ing it for every browser?
+  
+  //On the first click, start the timer.
+  if (syllableClickIdx == -1) {
+  }
 }
