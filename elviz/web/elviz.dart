6db67212e6b6bf62ccab230819447f404524be00
Alex Kersten <alex@dividebyxero.com>
2013-12-31 23:08:32 -0600

    About to move away from storing timecues and just keep track of the number of syllables

--- elviz/web/elviz.dart
+++ elviz/web/elviz.dart
@@ -1,5 +1,27 @@
 import 'dart:html';
 import 'dart:async';
+import 'dart:collection';
+
+/**
+ * Events will be used to drive the show. Every event has an appearance time
+ * associated with it, and will be triggered when that amount of time has
+ * elapsed.
+ */
+abstract class ElvizEvent {
+  var showtime = 0;
+  
+  ElvisEvent(var showtime) {
+    this.showtime = showtime;
+  }
+}
+
+class ShowTextEvent extends ElvizEvent {
+  String text;
+  
+  ShowTextEvent(var showtime, String text) : super(showtime) {
+    this.text = text;
+  }
+}
 
 //XXX: State for certain actions - right now things will break if the syllable
 //button action is triggered before things are actually set up (CSS will prevent
@@ -16,9 +38,18 @@ void main() {
     ..onClick.listen(syllableClick);
 }
 
+//The way this will work is that we'll start with an array of "tokens". Some
+//of these tokens might be multi-syllable (i.e. have *'s in them), that's ok.
+//We'll use the length of their respective innerTimecues list to track them as
+//we iterate through the symbols and record timecues. After recording each
+//timestamp, the event queue will be built with the ends of words trailed by a
+//space (the event queue will be a used for other things too, but these events
+//will just be "display n-characters" events).
 List<String> tokens = [];
-List<int> timecues = [];
-List<List<int>> innerTimecues = [];
+//List<int> timecues = [];
+//List<List<int>> innerTimecues = [];
+List<int> syllableCounts = [];
+Queue<ElvizEvent> eventQueue = new Queue<ElvizEvent>();
 
 void tapCues(MouseEvent event) {
   //Transform the textarea content into an array of strings delimited by space,
@@ -113,7 +144,17 @@ void syllableClick(MouseEvent event) {
   //Stop the timer after the last syllable happens.
   if (syllableClickIdx == tokens.length - 1) {
     stopwatch.stop();
+    
+    //TODO: Build the event chain from the timecues.
     return;
+  }
+  
+  if (innerTimecues[syllableClickIdx] == null) {
+    //Single syllable word.
+    timecues[syllableClickIdx] = 
+    syllableClickIdx++;
+  }else {
+    
   }
   
   
